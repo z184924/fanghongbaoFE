e9721cc12dea453b3bd0677535431b73526eb930
humreske <humreske@126.com>
2018-10-15 16:22:11 +0800

    f

--- src/pages/project/building/com/InputBox.vue
+++ src/pages/project/building/com/InputBox.vue
@@ -2,7 +2,6 @@
   <div>
     <el-form label-width="7em">
       <el-form-item v-for="(o,i) in inputList" :key="i" :label="o.label">
-
         <el-input type="textarea" autosize placeholder="请输入内容" v-model="o.value" @input="change($event,i)">
         </el-input>
       </el-form-item>
@@ -15,7 +14,7 @@ import clone from "clone"
 export default {
   props: {
     /*
-      key,label,type(default:text),
+      key,label
     */
     inputs: {
       default: []
@@ -30,7 +29,6 @@ export default {
   watch: {
     value: {
       handler() {
-        // console.log(this.value);
         this.setValue();
       },
       deep: true,
@@ -42,22 +40,18 @@ export default {
       this.inputList.forEach(o => {
         obj[o.key] = o.value;
       });
-      this.$emit("input", j.stringify(obj));
+      // console.log(JSON.stringify(obj));
+      this.$emit("input", JSON.stringify(obj));
     },
     setValue() {
       let v = {};
       try {
         v = j.parse(this.value);
       } catch (error) { }
-      // let list = this.inputs;
       this.inputList = clone(this.inputs);
       this.inputList.forEach((o, i) => {
         this.$set(this.inputList[i], "value", v[o.key]);
-        // o.value = v[o.key];
       });
-      // console.log(list);
-      // this.inputList = list;
-      // this.$forceUpdate();
     },
     change(e, index) {
       this.$set(this.inputList[index], "value", e);
